edc23a0b53b9c183ca3d0b832df8915b4a40e75a
epur4192 <epur4192@uni.sydney.edu.au>
2025-02-18 03:57:41 +1100

    Player Movement + Jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
-    public Keyboard keyboard;
-    public int speed = 10;
-    // Start is called before the first frame update
-    void Start() {
-        keyboard = Keyboard.current;
+    
+    // public Keyboard keyboard;
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+    private PlayerControls playerControls;
+
+    public int moveSpeed = 5;
+    public int jumpSpeed = 3;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        // PlayerControls is the name of the set of control you set 
+        playerControls = new PlayerControls();
+        playerControls.Player.Enable();
+        
+        // In this case: Player is the action map; Jump is the Actions 
+        // += in C# 'subscribes' the function with the event so multiple functions can be subscribed to the event
+        playerControls.Player.Jump.performed += Jump;
+        // playerControls.Player.Movement.performed += Move;
     }
 
-    // Update is called once per frame
-    void Update() {
-        if (keyboard == null) {
-            Debug.Log("Keyboard not found");
-        } 
+    void Start() {
+    }
 
-        if (keyboard.aKey.isPressed) {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-        } else if (keyboard.dKey.isPressed) {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
-        } 
-        if (keyboard.wKey.isPressed) {
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
+    public void Jump(InputAction.CallbackContext context) {
+        // Context tells you all the details of the input data
+        Debug.Log(context);
+        // Only one instance when button is pressed (Not when let go or started)
+        if (context.performed){
+            Debug.Log("Jump " + context.phase);
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
+
         }
     }
+
+    // public void Move(InputAction.CallbackContext context) {
+    //     Debug.Log(context);
+    //     Vector2 inputVector = context.ReadValue<Vector2>();
+    //     rb.AddForce(new Vector3(inputVector.x, 0 , inputVector.y) * moveSpeed, ForceMode2D.Force);
+    // }
+
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector2 inputVector = playerControls.Player.Movement.ReadValue<Vector2>();
+        rb.AddForce(new Vector3(inputVector.x, 0 , inputVector.y) * moveSpeed, ForceMode2D.Force);
+    }
+
+
 }
