12e2d95f990f51b26b622824b5f60257a650af2d
davadakus <ezekielpurwadi@gmail.com>
2025-02-26 21:54:32 +1100

    Aimarrow Working as Intended

--- Assets/Script/PlayerAim.cs
+++ Assets/Script/PlayerAim.cs
@@ -7,41 +7,25 @@ using UnityEngine.InputSystem;
 public class PlayerAim : MonoBehaviour
 {
     private PlayerControls playerControls;
-    private Vector3 mousePosition;
+
     // Start is called before the first frame update
     private void Awake() {
         // PlayerControls is the name of the set of control you set 
         playerControls = new PlayerControls();
         playerControls.Player.Enable();
-
     }
 
-        void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    //mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position);
-
-        
-    //}
-
+    // When playing with rotation, SCALE OF PARENT MUST BE AT 1,1,1 https://discussions.unity.com/t/stretching-model-when-rotating/847320/2
     private void FixedUpdate() {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(playerControls.Player.Aim.ReadValue<Vector2>());
-        //mousePosition.z = 0;
+        mousePosition.z = 0;
         Debug.Log("Mouse Vector: " + mousePosition);
         
-        Vector3 rotation = mousePosition - transform.position;
+        // normalized to keep it smooth
+        Vector3 rotation = (mousePosition - transform.position).normalized;
+        rotation.z = 0;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
     }
-    public Vector2 AsVector2(Vector3 _v) {
-        return new Vector2(_v.x, _v.y);
-    }
-
-
 }
